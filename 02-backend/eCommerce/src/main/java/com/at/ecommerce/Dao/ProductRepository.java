2a6f0722abf88ed4eda484db98218bd02ba7f728
Majid1395 <am-alattar@hotmail.com>
2024-05-30 13:57:39 +0200

    Test 1

---
+++ 02-backend/eCommerce/src/main/java/com/at/ecommerce/Dao/ProductRepository.java
@@ -0,0 +1,20 @@
+package com.at.ecommerce.Dao;
+
+import com.at.ecommerce.Entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin("http://localhost:4200")
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    // SELECT * FROM product where category_id =?
+    Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
+
+    // SELECT * FROM product p
+    // where
+    // p.name LIKE CONCAT('%', :name , '%')
+    Page<Product> findByNameContaining(@Param("name") String name, Pageable pageable);
+}
